cd6234751b70a318a8c16675a77b80898619b244
saikiran reddy <psai0987@gmail.com>
2021-10-31 10:06:30 +0000

    Delete provider.tf

--- terraform-aws-elb-alb/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 2.0"
-  AWS_ACCESS_KEY_ID = "AKIAYBX7CYEVWJ6R62UQ"
-  AWS_SECRET_ACCESS_KEY = "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"
-}
