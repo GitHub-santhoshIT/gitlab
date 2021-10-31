be86b66de3cfb04e4faee16e5135f6b2bfbd8485
saikiran reddy <psai0987@gmail.com>
2021-10-31 10:01:25 +0000

    Update provider.tf

--- terraform-aws-elb-alb/provider.tf
+++ terraform-aws-elb-alb/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "${var.region}"
   version = "~> 2.0"
-  access_key = ""AKIAYBX7CYEVWJ6R62UQ
-  secret_key = "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"
+  AWS_ACCESS_KEY_ID = ""AKIAYBX7CYEVWJ6R62UQ
+  AWS_SECRET_ACCESS_KEY = "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"
 }
